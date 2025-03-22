531045d8cd9c826e0653c01f09d4ca1226dcb6f8
Matissoss <vateuszptaszynski@proton.me>
2025-03-22 11:43:37 +0100

    made some changes to colorscheme

--- nvim/colorscheme.lua
+++ nvim/colorscheme.lua
@@ -1,59 +1,65 @@
 --	mateus' colorscheme
---======================
+--	-------------------
 --	made by matissoss
---	licensed under MIT License
+--	licensed under MIT
 
--- main colors
 local bg 		= "#121212"
 local bg_dark		= "#242424"
 
-local fg 		= "#C8C8CC"
-local fg_dark		= "#969699"
+-- this color is too bright for dark theme!
+--local fg 		= "#F5F5FA"
+local fg		= "#C8C8CC"
+local fg_dark		= "#AAAAAD"
 
 -- accent colors
 local red 		= "#C75147"
-local red_dark		= "#782830"
+local red_dark		= "#662A25"
 
 local blue 		= "#94D3E6"
-local blue_dark		= "#1D343C"
+local blue_dark		= "#6F9FAD"
 
-local cyan 		= "#A8FECB"
-local cyan_dark		= "#528064"
+local cyan 		= "#91FFE9"
+local cyan_dark		= "#71C7B6"
 
-local orange 		= "#FDA73C"
-local orange_dark	= "#D29A55"
+local orange 		= "#D99E64"
+local orange_dark	= "#B88654"
 
 local green 		= "#9BD85F"
 local green_dark 	= "#8DBF67"
 
-local yellow		= "#E3D050"
-local yellow_dark	= "#B8A840"
+local yellow		= "#D4B85D"
+local yellow_dark	= "#B39B4F"
 
 local magenta		= "#B157D0"
-local magenta_dark	= "#2F2433"
+local magenta_dark	= "#713885"
 
 -- start
 
-function set_color(group_name, bg_fg)
+function hl(group_name, bg_fg)
 	vim.api.nvim_set_hl(0, group_name, bg_fg)
 end
 
-set_color("Normal"	, { bg=bg,	fg=fg		})
-set_color("EndOfBuffer"	, { bg=bg,	fg=fg_dark	})
-set_color("Cursor"	, { bg=fg_dark,	fg=fg_dark	})
-set_color("CursorColumn", { bg=fg_dark			})
-set_color("Comment"	, { fg=fg_dark			})
+hl("Normal"		, { bg=bg,	fg=fg		})
+hl("EndOfBuffer"	, { bg=bg,	fg=fg_dark	})
+hl("Cursor"		, { bg=fg_dark,	fg=fg_dark	})
+hl("CursorColumn"	, { bg=fg_dark			})
+hl("Comment"		, { fg=fg_dark			})
 
-set_color("Statement"	, { fg=magenta			})
-set_color("Operator"	, { fg=fg_dark			})
-set_color("Type"	, { fg=fg			})
-set_color("Function"	, { fg=blue			})
-set_color("Identifier"	, { fg=green			})
-set_color("Constant"	, { fg=yellow			})
-set_color("Error"	, { fg=red			})
-set_color("Structure"	, { fg=red			})
-set_color("String"	, { fg=green_dark		})
-set_color("Directory"	, { fg=blue			})
-set_color("StatusLine"	, { bg=bg,	fg=fg_dark	})
-set_color("WarningMsg"	, { fg=yellow			})
-set_color("ErrorMsg"	, { fg=red			})
+hl("Delimeter"	, { fg=orange_dark		})
+hl("Character"	, { fg=green_dark		})
+hl("Boolean"	, { fg=orange_dark		})
+hl("Float"	, { fg=yellow			})
+hl("Keyword"	, { fg=magenta_dark		})
+hl("Statement"	, { fg=magenta			})
+hl("Operator"	, { fg=fg_dark			})
+hl("Type"	, { fg=fg			})
+hl("Function"	, { fg=orange			})
+hl("Identifier"	, { fg=green			})
+hl("Constant"	, { fg=yellow			})
+hl("Error"	, { fg=red			})
+hl("Structure"	, { fg=red			})
+hl("String"	, { fg=green_dark		})
+hl("Directory"	, { fg=orange			})
+hl("StatusLine"	, { bg=bg,	fg=fg_dark	})
+hl("WarningMsg"	, { fg=yellow			})
+hl("ErrorMsg"	, { fg=red			})
--- readme.md
+++ readme.md
@@ -9,7 +9,8 @@ This repo is central point for my colorscheme.
 # Table of contents
 
 - [Color Reference](#Color-Reference)
-- [All Colors](#All-Colors)
+- [Primary Colors](#Primary-Colors)
+- [Other Colors](#Other-Colors)
 - [Example Configs](#Examples)
 - [Plans](#Plans)
 
@@ -17,43 +18,51 @@ This repo is central point for my colorscheme.
 
 <img align=center src='color-ref.svg'>
 
-# All Colors
+# Primary Colors
 
 ## Black
-- `#121212`
-- `#242424`
+- `#121212` - background
+- `#242424` - brighter background
 
 ## White
-- `#C8C8CC`
-- `#969699`
+- `#F5F5FA` - white, too bright for dark themes.
+- `#C8C8CC` - white, perfect for darker themes.
+- `#AAAAAD` - dark white
 
 ## Red
-- `#C75147`
-- `#782830`
-
-## Blue
-- `#94D3E6`
-- `#1D343C`
-
-## Cyan
-- `#A8FECB`
-- `#528064`
+- `#C75147` - "satured dust red"
+- `#662A25` - "dark clay red"
 
 ## Orange
-- `#FDA73C`
-- `#D29A55`
-
-## Green
-- `#9BD85F`
-- `#8DBF67`
+- `#D99E64` - "dust orange"
+- `#B88654` - "dark dust orange"
 
 ## Yellow
-- `#E3D050`
-- `#B8A840`
+- `#D4B85D` - "goldish yellow"
+- `#B39B4F` - "dark goldish yellow"
 
 ## Magenta
-- `#E3D050`
-- `#2F2433`
+- `#B157D0` - "violet"
+- `#713885` - "dark violet"
+
+## Green (partially)
+- `#8DBF67` - "grass-like green"
+
+# Other Colors
+
+These are colors that are good to have.
+
+## Green
+- `#9BD85F` - "light grass-like green"
+- `#8DBF67` - "grass-like green"
+
+## Cyan
+- `#91FFE9` - "greenish sky-like blue"
+- `#71C7B6` - "dark greenish sky-like blue"
+
+## Blue
+- `#94D3E6` - "sky-like blue"
+- `#6F9FAD` - "dark sky-like blue"
 
 ---
 
@@ -61,7 +70,7 @@ This repo is central point for my colorscheme.
 
 - [neovim](nvim)
 - [alacritty](alacritty)
-- [wallpapers](https://github.com/Matissoss/mateus-wallpaper)
+- [wallpapers](https://github.com/Matissoss/mateus-wallpapers)
 
 # Contributing
 
@@ -69,7 +78,7 @@ If you want to submit your own theme for any CLI/program, create an Issue and I'
 
 # Plans
 
-I plan to revamp some colors in normal theme and make colorscheme for light theme. I also plan on making standalone neovim plugin for colorscheme.
+I plan to make colorscheme for light theme and add more configurations.
 
 # Credits
 
